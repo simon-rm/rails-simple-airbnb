a2c6cbcf647dd2df36bbb051a6ff366e275f8f6e
Simon R. Murcia <srisnikmurcia@itba.edu.ar>
2024-03-06 01:47:26 -0300

    added dumb styles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_06_031155) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_06_032524) do
   create_table "flats", force: :cascade do |t|
     t.string "name"
     t.string "address"
